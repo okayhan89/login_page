6b7f9c7ed55b117de3c70fb5e7f6bcfd90976177
lemon <lemon@DESKTOP-NQOOPGA>
2019-06-25 00:09:11 +0900

    login project

---
+++ daily-9/src/main/java/com/yeobi/daily/Daily9Application.java
@@ -0,0 +1,13 @@
+package com.yeobi.daily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Daily9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Daily9Application.class, args);
+	}
+
+}
---
+++ daily-9/src/main/java/com/yeobi/daily/MemberController.java
@@ -0,0 +1,9 @@
+package com.yeobi.daily;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MemberController {
+
+	
+}
